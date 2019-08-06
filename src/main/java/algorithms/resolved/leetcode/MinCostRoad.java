7f35150b7be97d37d2ea1f9766d64558b4193cf0
edmarr <edmardosreis@gmail.com>
2019-08-06 14:01:39 -0300

    update code,  but  is not working.

--- src/main/java/algorithms/resolved/leetcode/MinCostRoad.java
+++ src/main/java/algorithms/resolved/leetcode/MinCostRoad.java
@@ -4,41 +4,72 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.Set;
 
 public class MinCostRoad {
-	public Integer getMinCostToConstruct(int numCities, int totalAvailableCities, int[][] roadsAvailable, int roadsToBeRepaired, int[][] costRoadsToBeRepaired) {
+	Map<Integer, Integer> cities = new HashMap<>();
+
+	public Integer getMinCostToConstruct(int numCities, int totalAvailableCities, int[][] roadsAvailable,
+			int roadsToBeRepaired, int[][] costRoadsToBeRepaired) {
 		int cost = 0;
 		if (numCities < 1) {
 			return cost;
 		}
-
-		int components = numCities;
-		int[] parents = new int[components + 1];
-
-		for (int i = 0; i < parents.length; i++) {
-			parents[i] = i;
+		int visit = numCities;
+		while (numCities > 0) {
+			cities.put(numCities, numCities);
+			numCities--;
 		}
-		
-		
-		PriorityQueue<int []> queue = new PriorityQueue<int[]>(new Comparator<int[]>() {
-			public int compare(int [] a , int [] b) {
+
+		for (int[] route : roadsAvailable) {
+			System.out.println(route[0] + " x > y " + route[1]);
+			union(route[0], route[1]);
+			visit--;
+		}
+
+		PriorityQueue<int[]> queue = new PriorityQueue<int[]>(new Comparator<int[]>() {
+			public int compare(int[] a, int[] b) {
 				return a[2] - b[2];
 			}
-			
 		});
-		
-		Map<Integer , Set<Integer>> map = new HashMap<>();
-		Set<Integer> set = null;
-		
-		
-		for(int[] damaged : costRoadsToBeRepaired) {
-			
+
+		for (int[] route : costRoadsToBeRepaired) {
+			queue.add(route);
 		}
 		
-		
+		while(visit != 0 &&  queue.size() > 0) {
+			int[] poll = queue.poll();
+			int x = getParent(poll[0]);
+			int y = getParent(poll[1]);
+			int value = getParent(2);
+			
+			if(x != y) {
+				union(x, y);
+				cost = cost+ value;
+			}
+			visit--;
+			
+		}
 
-		return null;
+		return cost;
+	}
+
+	private void union(int i, int j) {
+		int x = getParent(i);
+		int y = getParent(j);
+		
+		System.out.println( y +  " union " +  x );
+		
+		if (x != y) {
+			cities.put(y,x);
+		}
+	}
+
+	private Integer getParent(Integer i) {
+        if(cities.get(i) != i){
+            cities.put(i, getParent(cities.get(i)));
+        }
+        return cities.get(i);
+        
 	}
 
 }
