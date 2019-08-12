8a32b1a682c1dd63c0faf1f1f17dfef23fcce542
edmarr <edmardosreis@gmail.com>
2019-08-12 13:50:17 -0300

    Learn new Algorithm .

--- src/main/java/algorithms/resolved/leetcode/CutOffTreesforGolfEvent.java
+++ src/main/java/algorithms/resolved/leetcode/CutOffTreesforGolfEvent.java
@@ -1,15 +1,18 @@
 package algorithms.resolved.leetcode;
 
-import java.util.HashSet;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
-
+import java.util.Queue;
 
 /**
  * https://leetcode.com/problems/cut-off-trees-for-golf-event/submissions/
  *
  */
 public class CutOffTreesforGolfEvent {
+
+	private static int directions[][] = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
 
 	public int cutOffTree(List<List<Integer>> forest) {
 
@@ -21,68 +24,85 @@ public class CutOffTreesforGolfEvent {
 			return -1;
 		}
 
-		Set<Integer> trees = getCuts(forest);
+		List<int[]> allTrees = getAllTrees(forest);
 
-		Boolean isLeft = false;
-		Boolean isRight = true;
-		int x = 0;
-		int y = 0;
-		int cut = 0;
+		Collections.sort(allTrees, new Comparator<int[]>() {
 
-		while (!trees.isEmpty()) {
-			if (isSecureRight(forest, x, y) && isRight) {
-				cut++;
-				y++;
-				isLeft = false;
-				 System.out.println(forest.get(x).get(y));
-				trees.remove(forest.get(x).get(y));
-			} else if (isSecureLeft(forest, x, y) && isLeft) {
-				cut++;
-				y--;
-				isRight = false;
-				 System.out.println(forest.get(x).get(y));
-				trees.remove(forest.get(x).get(y));
-			} else if (isSecureDown(forest, x, y)) {
-				x++;
-				cut++;
-				isRight = true;
-				isLeft = true;
-				 System.out.println(forest.get(x).get(y));
-				trees.remove(forest.get(x).get(y));
-			} else {
-				break;
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[1] - o1[2];
 			}
+		});
+
+		int minSteps = 0;
+		int curX = 0;
+		int curY = 0;
+		while (!allTrees.isEmpty()) {
+			int[] curTree = allTrees.remove(0);
+			int steps = getMinSteps(forest, curX, curY, curTree[0], curTree[1]);
+			if (steps == -1) {
+				return -1;
+			}
+
+			minSteps += steps;
+			curX = curTree[0];
+			curY = curTree[1];
+
 		}
 
-		if (trees.size() > 1) {
-			return -1;
-		}
-
-		return cut;
+		return minSteps;
 	}
 
-	private boolean isSecureRight(List<List<Integer>> forest, int x, int y) {
-		return y + 1 < forest.get(x).size() && !forest.get(x).get(y + 1).equals(0);
-	}
+	private int getMinSteps(List<List<Integer>> forest, int curX, int curY, int valueX, int valueY) {
+		int minSteps = 0;
+		int rows = forest.size();
+		int cols = forest.get(0).size();
+		boolean[][] visited = new boolean[rows][cols];
+		Queue<int[]> queue = new LinkedList<int[]>();
 
-	private boolean isSecureLeft(List<List<Integer>> forest, int x, int y) {
-		return y - 1 >= 0 && !forest.get(x).get(y - 1).equals(0);
-	}
+		queue.offer(new int[] { curX, curY });
+		visited[curX][curY] = true;
 
-	private boolean isSecureDown(List<List<Integer>> forest, int x, int y) {
-		return x + 1 < forest.size() && !forest.get(x+1).get(y).equals(0);
-	}
-
-	private Set<Integer> getCuts(List<List<Integer>> forest) {
-		Set<Integer> set = new HashSet<Integer>();
-		for (List<Integer> list : forest) {
-			for (Integer value : list) {
-				if (!value.equals(0)) {
-					set.add(value);
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				int[] cell = queue.poll();
+				if(cell[0] == valueX && cell[1] == valueY) {
+					return minSteps;
+				}
+				for(int [] direction : directions) {
+					int nx = cell[0] + direction[0];
+					int ny = cell[1] + direction[1];
+					if(nx >= 0 && nx < rows && ny>0 && ny < cols && !visited[nx][ny] && forest.get(nx).get(ny) != 0) {
+						queue.add(new int[] {nx , ny});
+						visited[nx][ny] = true;
+					}
+					
 				}
 			}
+			minSteps++;
 		}
-		return set;
+
+		return -1;
 	}
 
+	private List<int[]> getAllTrees(List<List<Integer>> forest) {
+		List<int[]> list = new LinkedList<>();
+		for (int i = 0; i < forest.size(); i++) {
+			for (int j = 0; j < forest.get(i).size(); j++) {
+				int value = forest.get(i).get(j);
+				if (value > 1) {
+					int element[] = new int[3];
+					element[0] = i;
+					element[1] = j;
+					element[2] = value;
+					list.add(element);
+				}
+
+			}
+		}
+
+		return list;
+
+	}
 }
