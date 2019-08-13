e8f5c511ca566263b8e28fb4f910dd97f0463444
edmarr <edmardosreis@gmail.com>
2019-08-13 13:42:05 -0300

    Update leetcode

--- src/main/java/algorithms/resolved/leetcode/CutOffTreesforGolfEvent.java
+++ src/main/java/algorithms/resolved/leetcode/CutOffTreesforGolfEvent.java
@@ -12,88 +12,83 @@ import java.util.Queue;
  */
 public class CutOffTreesforGolfEvent {
 
-	private static int directions[][] = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+	private static int[][] directions = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
 
 	public int cutOffTree(List<List<Integer>> forest) {
-
-		List<int[]> allTrees = getAllTrees(forest);
-
-		Collections.sort(allTrees, new Comparator<int[]>() {
+		int minSteps = 0;
+		List<int[]> treeHeights = getAllTreeHights(forest);
+		Collections.sort(treeHeights, new Comparator<int[]>() {
 			@Override
 			public int compare(int[] o1, int[] o2) {
 				return o1[2] - o2[2];
 			}
 		});
 
-		int minSteps = 0;
-		int curX = 0;
-		int curY = 0;
-		while (!allTrees.isEmpty()) {
-			int[] curTree = allTrees.remove(0);
-			int steps = getMinSteps(forest, curX, curY, curTree[0], curTree[1]);
+		int x = 0;
+		int y = 0;
+		while (!treeHeights.isEmpty()) {
+			int[] curr = treeHeights.remove(0);
+			int steps = getMinSteps(forest, x, y, curr[0], curr[1]);
 			if (steps == -1) {
 				return -1;
 			}
-
 			minSteps += steps;
-			curX = curTree[0];
-			curY = curTree[1];
-
+			x = curr[0];
+			y = curr[1];
+			forest.get(x).set(y, 1);
 		}
 
 		return minSteps;
 	}
 
-	private int getMinSteps(List<List<Integer>> forest, int curX, int curY, int valueX, int valueY) {
-		int minSteps = 0;
+	private int getMinSteps(List<List<Integer>> forest, int x, int y, int valueX, int valueY) {
+		int steps = 0;
 		int rows = forest.size();
 		int cols = forest.get(0).size();
-		boolean[][] visited = new boolean[rows][cols];
+		boolean visited[][] = new boolean[rows][cols];
+		visited[x][y] = true;
 		Queue<int[]> queue = new LinkedList<int[]>();
-
-		queue.offer(new int[] { curX, curY });
-		visited[curX][curY] = true;
+		queue.offer(new int[] { x, y });
 
 		while (!queue.isEmpty()) {
 			int size = queue.size();
 			for (int i = 0; i < size; i++) {
-				int[] cell = queue.poll();
-				if (cell[0] == valueX && cell[1] == valueY) {
-					return minSteps;
+				int tree[] = queue.poll();
+
+				if (tree[0] == valueX && tree[1] == valueY) {
+					return steps;
 				}
 				for (int[] direction : directions) {
-					int nx = cell[0] + direction[0];
-					int ny = cell[1] + direction[1];
-					if (nx >= 0 && nx < rows && ny > 0 && ny < cols && !visited[nx][ny]	&& forest.get(nx).get(ny) != 0) {
+					int nx = tree[0] + direction[0];
+					int ny = tree[1] + direction[1];
+
+					if (nx >= 0 && nx < rows && ny >= 0 && ny < cols && !visited[nx][ny]
+							&& forest.get(nx).get(ny) != 0) {
 						queue.add(new int[] { nx, ny });
 						visited[nx][ny] = true;
 					}
 
 				}
+
 			}
-			minSteps++;
+
+			steps++;
 		}
 
 		return -1;
 	}
 
-	private List<int[]> getAllTrees(List<List<Integer>> forest) {
-		List<int[]> list = new LinkedList<>();
+	private List<int[]> getAllTreeHights(List<List<Integer>> forest) {
+		List<int[]> list = new LinkedList<int[]>();
 		for (int i = 0; i < forest.size(); i++) {
 			for (int j = 0; j < forest.get(i).size(); j++) {
-				int value = forest.get(i).get(j);
-				if (value > 1) {
-					int element[] = new int[3];
-					element[0] = i;
-					element[1] = j;
-					element[2] = value;
-					list.add(element);
+				int temp = forest.get(i).get(j);
+				if (temp > 1) {
+					int[] tree = new int[] { i, j, temp };
+					list.add(tree);
 				}
-
 			}
 		}
-
 		return list;
-
 	}
 }
