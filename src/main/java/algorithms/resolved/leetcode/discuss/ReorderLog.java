0acde3c9755ca9ad9ec638facd3f9ef8b836ada6
Edmar dos reis <edmar.silva@sicoob.com.br>
2019-10-09 11:54:02 -0300

    Structure to Reorder

---
+++ src/main/java/algorithms/resolved/leetcode/discuss/ReorderLog.java
@@ -0,0 +1,28 @@
+package algorithms.resolved.leetcode.discuss;
+
+public class ReorderLog {
+
+	private String[] reorder(String [] arr) {
+		
+		
+		
+		return arr;
+		
+	}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		String [] arr = new String [] {"dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"};
+		
+		
+	}
+	
+}
