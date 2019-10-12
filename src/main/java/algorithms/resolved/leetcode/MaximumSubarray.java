f71d6bce27580ed281fb775652d54e797518107f
edmarr <edmardosreis@gmail.com>
2019-10-12 17:50:54 -0300

    Max sub arrays sum LeetCode

---
+++ src/main/java/algorithms/resolved/leetcode/MaximumSubarray.java
@@ -0,0 +1,19 @@
+package algorithms.resolved.leetcode;
+
+public class MaximumSubarray {
+	public int max(int[] nums) {
+        int max = nums[0];
+        int sum = nums[0];
+        for(int i =1 ; i < nums.length ; i++){
+            sum = Math.max(sum + nums[i], nums[i]);
+            max = Math.max(max, sum);
+        }
+
+        return max;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = new int [] {-2,1,-3,4,-1,2,1,-5,4};
+		System.out.println(new MaximumSubarray().max(arr));
+	}
+}
