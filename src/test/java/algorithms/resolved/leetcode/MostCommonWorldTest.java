9cd993979b335b5419c27e7e4544e9f7bee544df
Edmar dos Reis <edmar.dos.reis.silva@sap.com>
2020-11-27 11:26:13 +0100

    remove vulnerabilility junit

--- src/test/java/algorithms/resolved/leetcode/MostCommonWorldTest.java
+++ src/test/java/algorithms/resolved/leetcode/MostCommonWorldTest.java
@@ -4,8 +4,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import algorithms.resolved.leetcode.MostCommonWorld;
-
 public class MostCommonWorldTest {
 	private MostCommonWorld problem;
 
